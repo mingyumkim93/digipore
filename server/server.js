71954788a680cf01d400aac85d1341b0a4e89814
mingyumkim93 <mingyumkim93@gmail.com>
2019-07-23 23:55:02 +0300

    made refreshing is possible. (but after refresh, auth.isauthenticated goes false -> always login page after refresh)

--- server/server.js
+++ server/server.js
@@ -4,7 +4,19 @@ var app = express();
 app.use(express.static("../wwwroot"));
 app.use(bodyParser.json());
 
+
 let db = require("./mysqlhelper");
 let requestApi = require("./requestapi");
 requestApi(app,db);
 app.listen(9000);
+
+
+path = require("path");
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, '../wwwroot/index.html'), function(err) {
+      if (err) {
+        res.status(500).send(err)
+      }
+    })
+  })
+  
